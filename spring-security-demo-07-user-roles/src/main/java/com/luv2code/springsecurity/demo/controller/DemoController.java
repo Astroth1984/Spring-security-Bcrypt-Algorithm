2ad51894a136af2a0efded6d3282fc917e096c1d
Astroth1984 <elmounir.othmane666@gmail.com>
2023-01-01 19:27:30 +0100

    User roles routing

---
+++ spring-security-demo-07-user-roles/src/main/java/com/luv2code/springsecurity/demo/controller/DemoController.java
@@ -0,0 +1,29 @@
+package com.luv2code.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	
+	@GetMapping("/")
+	public String showHome() {
+		
+		return "home";
+	}
+	
+	// Request Mapping for 
+	@GetMapping("/leaders")
+	public String showLeaders() {
+		
+		return "leaders";
+	}
+	
+	// RequestMapping for /systems
+	@GetMapping("/systems")
+	public String showSystems() {
+		
+		return "systems";
+	}
+
+}
---
+++ spring-security-demo-07-user-roles/src/main/webapp/WEB-INF/view/access-denied.jsp
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+	<title>CodXdoC - Access-denied</title>
+</head>
+
+<body>
+
+	<h2>Access Denied - You are not Authorized to access this resource.</h2>
+	
+	<hr>
+	
+	<a href="${pageContext.request.contextPath}/">Home Page</a>
+
+</body>
+
+</html>
